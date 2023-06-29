bb5e4c427940e021b7074a1c7b73128cc5a5a25e
Otto Kyösti (home) <otto.t.kyosti@tuni.fi>
2023-06-29 22:07:06 +0300

    Quick compiler error fix

--- GameCampRPG/Assets/Code/Combat/CombatPlayerUnit.cs
+++ GameCampRPG/Assets/Code/Combat/CombatPlayerUnit.cs
@@ -1,5 +1,5 @@
 using GameCampRPG.UI;
-using Packages.Rider.Editor.UnitTesting;
+//using Packages.Rider.Editor.UnitTesting;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
